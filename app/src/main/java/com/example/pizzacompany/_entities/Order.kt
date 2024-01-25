55bbb12ca68f366ea438d9bae3a9834706a79ce3
Fernando Salom <fer@rudo.es>
2024-01-25 11:56:48 +0100

    Fix order params

--- app/src/main/java/com/example/pizzacompany/_entities/Order.kt
+++ app/src/main/java/com/example/pizzacompany/_entities/Order.kt
@@ -3,6 +3,6 @@ package com.example.pizzacompany._entities
 import com.example.pizzacompany._information.MenuPizza
 
 data class Order(
-    val pizzas: List<MenuPizza>,
-    val total: Float = 0.0f
+    val pizzas: MutableList<MenuPizza> = mutableListOf<MenuPizza>(),
+    var total: Double = 0.0
 )
